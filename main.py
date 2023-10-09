d51324c44a4428e5f9d87a8d6f8b5aafd2d6e0cb
Eran Chetz <eran@chetz.org>
2023-10-09 23:18:01 +0300

    Update main.py
    get a list of UserIDs from command line arguments
    get SLACK_USER_TOKEN from environment variable

--- main.py
+++ main.py
@@ -1,18 +1,40 @@
+import sys
+import os
 from slack_sdk import WebClient
 
-# Initialize a Web API client
-client = WebClient(token='your-user-token-here')
+def set_status(client, user_id, status_text, status_emoji, status_expiration):
+    response = client.users_profile_set(
+        user=user_id,
+        profile={
+            "status_text": status_text,
+            "status_emoji": status_emoji,
+            "status_expiration": status_expiration
+        }
+    )
 
-# Set the user's status
-response = client.users_profile_set(
-    user='user-id-here',  # User ID of the employee
-    profile={
-        "status_text": "Out of Office",
-        "status_emoji": ":house:",
-        "status_expiration": 0  # Status does not expire
-    }
-)
+    if not response['ok']:
+        print(f"Error for {user_id}: {response['error']}")
+    else:
+        print(f"Status set for {user_id}")
 
-# Check for errors
-if not response['ok']:
-    print(f"Error: {response['error']}")
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <user-id-1> <user-id-2> ...")
+        sys.exit(1)
+
+    token = os.getenv('SLACK_USER_TOKEN')
+    if not token:
+        print("Error: SLACK_USER_TOKEN environment variable not set")
+        sys.exit(1)
+
+    client = WebClient(token=token)
+
+    status_text = "Out of Office"
+    status_emoji = ":house:"
+    status_expiration = 0
+
+    for user_id in sys.argv[1:]:
+        set_status(client, user_id, status_text, status_emoji, status_expiration)
+
+if __name__ == "__main__":
+    main()
